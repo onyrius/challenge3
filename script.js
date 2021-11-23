cb9083e231e48f996de22c5d886a76fec3973d94
Suelen <suelenonyrius@gmail.com>
2021-11-23 06:32:33 -0300

    add requisito 7.

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const pixelBoard = document.getElementById('pixel-board');
-let main = document.getElementsByTagName('main')[0];
-let eachPixelList =['', '', '', '', '', '', '', '', '', '', '', '', '', '', '','', '', '', '', '', '', '', '', '', ''];
+const colorPalete = document.getElementById('#color-palete');
+console.log(colorPalete);
+let pixel = document.querySelector('#pixel');
+let colors = document.querySelectorAll('.color');console.log(colors);
+let eachPixelList = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '','', '', '', '', '', '', '', '', '', ''];
+let colorBlack = document.querySelector('#black-1');
+let colorRed = document.querySelector('#red-2');
+let colorGreen = document.querySelector('#green-3');
+let colorBlue = document.querySelector('#blue-4')
 
 for (let index = 0; index < eachPixelList.length; index += 1) {
   let eachPixel = eachPixelList[index];
+  let pixel1 = document.createElement('p');
+  eachPixelList.innerText = pixel1;
+  pixel1.className = 'pixel';
+  pixelBoard.appendChild(pixel1);
+}
 
-  let pixel = document.createElement('p');
-
-  eachPixelList.innerText = pixel;
-  pixel.className = 'pixel';
-  pixelBoard.appendChild(pixel);
+for (const color of colors) {
+  color.addEventListener('click', selectedColor)
+}
+function selectedColor(event) {
+  for (const color of colors) {
+    color.classList.remove('selected');
+    event.target.classList.add('selected');
+    console.log(event.target);
+ }
 }
