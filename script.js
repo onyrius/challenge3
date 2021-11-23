94f59276b1072b9cfd5ed544086a4b6bcff47d67
Suelen <suelenonyrius@gmail.com>
2021-11-23 12:00:20 -0300

    add soluçoes parciais para os requisitos 8 e 9

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const pixelBoard = document.getElementById('pixel-board');
 const colorPalete = document.getElementById('#color-palete');
-console.log(colorPalete);
-let pixel = document.querySelector('#pixel');
+let pixel = document.getElementsByClassName('pixel');
+console.log(pixel);
 let colors = document.querySelectorAll('.color');console.log(colors);
 let eachPixelList = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '','', '', '', '', '', '', '', '', '', ''];
-let colorBlack = document.querySelector('#black-1');
-let colorRed = document.querySelector('#red-2');
-let colorGreen = document.querySelector('#green-3');
-let colorBlue = document.querySelector('#blue-4')
+const colorBlack = document.querySelector('#black-1');
+const colorRed = document.querySelector('#red-2');
+const colorGreen = document.querySelector('#green-3');
+const colorBlue = document.querySelector('#blue-4');
 
 for (let index = 0; index < eachPixelList.length; index += 1) {
   let eachPixel = eachPixelList[index];
-  let pixel1 = document.createElement('p');
+ const pixel1 = document.createElement('p');
   eachPixelList.innerText = pixel1;
   pixel1.className = 'pixel';
   pixelBoard.appendChild(pixel1);
@@ -25,5 +25,22 @@ function selectedColor(event) {
     color.classList.remove('selected');
     event.target.classList.add('selected');
     console.log(event.target);
+ }
+}
+
+for (let place of pixel) {
+ place.addEventListener('click', paintColor);
+  function paintColor (evento) {
+   if (selectedColor === colorRed) {
+    selectedColor.classList.remove('color selected')
+    evento.target.classList.add('color selected')
+    place.style.backgroundColor = 'red'
+   } if (selectedColor === colorGreen) {
+    place.style.backgroundColor = 'green'
+   } if (selectedColor === colorBlue) {
+    place.style.backgroundColor = 'blue'
+   } if (selectedColor === colorBlack){
+   place.style.backgroundColor = 'black'
+   }
  }
 }
