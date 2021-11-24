b0dac59bb0a5fd9f20ecfb3a4080d1b7d9976a7c
Suelen <suelenonyrius@gmail.com>
2021-11-24 15:42:11 -0300

    recomeçando o projeto Pixels Art

--- script.js
+++ script.js
@@ -1,46 +1,32 @@
-const pixelBoard = document.getElementById('pixel-board');
-const colorPalete = document.getElementById('#color-palete');
-let pixel = document.getElementsByClassName('pixel');
-console.log(pixel);
-let colors = document.querySelectorAll('.color');console.log(colors);
-let eachPixelList = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '','', '', '', '', '', '', '', '', '', ''];
-const colorBlack = document.querySelector('#black-1');
-const colorRed = document.querySelector('#red-2');
-const colorGreen = document.querySelector('#green-3');
-const colorBlue = document.querySelector('#blue-4');
+//-------------------------------------------
+//criando o pixel board
+//-------------------------------------------
+const pixelBoard = document.querySelector('#pixel-board');
+const pixel = document.createElement('div')
+pixel.classList = 'pixel'
+for (let index = 0; index <25; index += 1) {
+  pixelBoard.innerHTML += '<div class="pixel"></div>';
+}
+/**Source https://www.ti-enxame.com/pt/javascript/crie-varias-divs-usando-um-loop/1053998065/ */
 
-for (let index = 0; index < eachPixelList.length; index += 1) {
-  let eachPixel = eachPixelList[index];
- const pixel1 = document.createElement('p');
-  eachPixelList.innerText = pixel1;
-  pixel1.className = 'pixel';
-  pixelBoard.appendChild(pixel1);
+//-------------------------------------------
+//selecionar cor da paleta de cor pelo clique
+//-------------------------------------------
+let colors = document.querySelectorAll('.color')
+for (let index = 0; index < colors.length; index++) {
+  let color = colors[index];
+  color.addEventListener('click' , selectColor);  
 }
+function selectColor(selecionar) {
+  for( let color of colors)
+  color.classList.remove('selected')
+    selecionar.target.classList.add('selected')
+    let colorToPaint = selecionar.target
+    return colorToPaint
+}
+console.log(typeof selectColor)
 
-for (const color of colors) {
-  color.addEventListener('click', selectedColor)
-}
-function selectedColor(event) {
-  for (const color of colors) {
-    color.classList.remove('selected');
-    event.target.classList.add('selected');
-    console.log(event.target);
- }
-}
-
-for (let place of pixel) {
- place.addEventListener('click', paintColor);
-  function paintColor (evento) {
-   if (selectedColor === colorRed) {
-    selectedColor.classList.remove('color selected')
-    evento.target.classList.add('color selected')
-    place.style.backgroundColor = 'red'
-   } if (selectedColor === colorGreen) {
-    place.style.backgroundColor = 'green'
-   } if (selectedColor === colorBlue) {
-    place.style.backgroundColor = 'blue'
-   } if (selectedColor === colorBlack){
-   place.style.backgroundColor = 'black'
-   }
- }
-}
+//------------------------------------------------------------
+//pintar pixels do pixel-board com as cores da paleta de cores
+//------------------------------------------------------------
+let black = document.querySelector('#black');
